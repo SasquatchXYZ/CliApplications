568583eb70b6fb06ea6738fc65da47c3140fe5be
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-04-13 17:32:48 -0400

    Add `Add` functionality

--- Bookmarkr/Program.cs
+++ Bookmarkr/Program.cs
@@ -1,9 +1,48 @@
-﻿namespace Bookmarkr;
+using static Bookmarkr.Utilities.Helper;
+using Bookmarkr.Services;
+
+namespace Bookmarkr;
 
 class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[]? args)
     {
-        Console.WriteLine("Hello, World!");
+        if (args is null || args.Length == 0)
+        {
+            ShowErrorMessage(["You haven't passed any argument.  The expected syntax is:", "bookmarkr <command-name> <parameters>"]);
+            return;
+        }
+
+        var service = new BookmarkService();
+
+        switch (args[0].ToLower())
+        {
+            case "link":
+                ManageLinks(args, service);
+                break;
+            // We may add more commands here...
+            default:
+                ShowErrorMessage(["Unknown Command"]);
+                break;
+        }
+    }
+
+    private static void ManageLinks(string[] args, BookmarkService service)
+    {
+        if (args.Length < 2)
+        {
+            ShowErrorMessage(["Insufficient number of parameters.  The expected syntax is:", "bookmarkr link <subcommand> <parameters>"]);
+        }
+
+        switch (args[1].ToLower())
+        {
+            case "add":
+                service.AddLink(args[2], args[3]);
+                break;
+            // We may add more subcommands here...
+            default:
+                ShowErrorMessage(["Insufficient number of parameters.  The expected syntax is:", "bookmarkr link <subcommand> <parameters>"]);
+                break;
+        }
     }
 }
--- Bookmarkr/Services/BookmarkService.cs
+++ Bookmarkr/Services/BookmarkService.cs
@@ -1,8 +1,33 @@
+using static Bookmarkr.Utilities.Helper;
 using Bookmarkr.Models;
 
 namespace Bookmarkr.Services;
 
 public class BookmarkService
 {
-    private readonly List<Bookmark> _bookMarks = [];
+    private readonly List<Bookmark> _bookmarks = [];
+
+    public void AddLink(string name, string url)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            ShowErrorMessage(["The 'name' for the link is not provided.  The expected syntax is:", "bookmarkr link add <name> <url>"]);
+            return;
+        }
+
+        if (string.IsNullOrWhiteSpace(url))
+        {
+            ShowErrorMessage(["The 'url' for the link is not provided.  The expected syntax is:", "bookmarkr link add <name> <url>"]);
+            return;
+        }
+
+        if (_bookmarks.Any(bookmark => bookmark.Name.Equals(name, StringComparison.OrdinalIgnoreCase)))
+        {
+            ShowWarningMessage([$"A link with the name '{name}' already exists.  It will not be added", $"To update the existing link, use the command: bookmarkr link update '{name}' '{url}'"]);
+            return;
+        }
+
+        _bookmarks.Add(new Bookmark { Name = name, Url = url });
+        ShowSuccessMessage(["Bookmark added successfully."]);
+    }
 }
