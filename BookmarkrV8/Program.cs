23c7a9347b474f6e91fd421cb2fa09471f2ad370
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-04-20 18:28:10 -0400

    Refactor InteractiveCommand

--- BookmarkrV8/Program.cs
+++ BookmarkrV8/Program.cs
@@ -1,11 +1,10 @@
-﻿using Spectre.Console;
-using System.CommandLine;
+﻿using System.CommandLine;
 using System.CommandLine.Builder;
 using System.CommandLine.Hosting;
 using System.CommandLine.Parsing;
-using System.Text.Json;
 using BookmarkrV8.Commands.Export;
 using BookmarkrV8.Commands.Import;
+using BookmarkrV8.Commands.Interactive;
 using BookmarkrV8.Commands.Link;
 using BookmarkrV8.Services.BookmarkService;
 using Microsoft.Extensions.Configuration;
@@ -37,29 +36,15 @@ class Program
         rootCommand.AddCommand(new ExportCommand(_bookmarkService, "export", "Exports all bookmarks to a designated output file"));
         rootCommand.AddCommand(new ImportCommand(_bookmarkService, "import", "Imports all bookmarks from a designated file"));
         rootCommand.AddCommand(new LinkCommand(_bookmarkService, "link", "Manage bookmarks links"));
+        rootCommand.AddCommand(new InteractiveCommand(_bookmarkService, "interactive", "Manage bookmarks interactively"));
 
-        var interactiveCommand = new Command("interactive", "Manage bookmarks interactively");
-        rootCommand.AddCommand(interactiveCommand);
-        interactiveCommand.SetHandler(OnInteractiveCommand);
-
+        // The Builder Pattern
         var parser = new CommandLineBuilder(rootCommand)
             .UseHost(_ => Host.CreateDefaultBuilder(),
                 builder =>
                 {
                     builder.ConfigureServices(services =>
                     {
-                        // ** Configuration in Code **
-                        // services.AddSerilog(config =>
-                        // {
-                        //     config.MinimumLevel.Information();
-                        //     config.WriteTo.Console();
-                        //     config.WriteTo.File("logs/bookmarkr-.txt",
-                        //         rollingInterval: RollingInterval.Day,
-                        //         restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Error);
-                        //
-                        //     config.CreateLogger();
-                        // });
-
                         // ** Configuration from appsettings.json **
                         services.AddSerilog(config =>
                         {
@@ -77,150 +62,25 @@ class Program
 
         return await parser.InvokeAsync(args);
 
-        // Handler Methods
+        // Handler for the Root Command
         static void OnHandleRootCommand()
         {
             Console.WriteLine("Hello from the root command!");
-        }
-
-        void OnInteractiveCommand()
-        {
-            var isRunning = true;
-            while (isRunning)
-            {
-                AnsiConsole.Write(
-                    new FigletText("Bookmarkr")
-                        .Centered()
-                        .Color(Color.SteelBlue)
-                );
-
-                var selectedOperation = AnsiConsole.Prompt(
-                    new SelectionPrompt<string>()
-                        .Title("[blue]What do you want to do?[/]")
-                        .AddChoices([
-                            "Export Bookmarks To File",
-                            "View Bookmarks",
-                            "Exit Program"
-                        ])
-                );
-
-                switch (selectedOperation)
-                {
-                    case "Export Bookmarks To File":
-                        ExportBookmarks();
-                        break;
-                    case "View Bookmarks":
-                        ViewBookmarks();
-                        break;
-                    default:
-                        isRunning = false;
-                        break;
-                }
-            }
-        }
-
-        void ExportBookmarks()
-        {
-            // Ask for the outputFilePath
-            var outputFilePath = AnsiConsole.Prompt(
-                new TextPrompt<string>("Please provide the output file name (default: 'bookmarks.json')")
-                    .DefaultValue("bookmarks.json"));
-
-            // Export the bookmarks to the specified file, while showing progress.
-            AnsiConsole.Progress()
-                .AutoRefresh(true) // Turns on auto refresh
-                .AutoClear(false) // Avoids removing the task list when completed
-                .HideCompleted(false) // Avoids hiding tasks as they are completed
-                .Columns(
-                [
-                    new TaskDescriptionColumn(), // Shows the task description
-                    new ProgressBarColumn(), // Shows the progress bar
-                    new PercentageColumn(), // Shows the current percentage
-                    new RemainingTimeColumn(), // Shows the remaining time
-                    new SpinnerColumn(), // Shows the spinner, indicating that the operation is ongoing
-                ])
-                .Start(ctx =>
-                {
-                    // Get the list of all bookmarks
-                    var bookmarks = _bookmarkService.GetAll();
-
-                    // Export the bookmarks to file
-                    // 1. Create the task
-                    var task = ctx.AddTask("[yellow]Exporting all bookmarks to file...[/]");
-
-                    // 2. Set the total steps for the progress bar
-                    task.MaxValue = bookmarks.Count;
-
-                    // 3. Open the file for writing
-                    using (StreamWriter writer = new StreamWriter(outputFilePath))
-                    {
-                        while (!ctx.IsFinished)
-                        {
-                            foreach (var bookmark in bookmarks)
-                            {
-                                // 3.1 Serialize the current bookmark as JSON and write to the file asynchronously
-                                writer.WriteLine(JsonSerializer.Serialize(bookmark));
-
-                                // 3.2. Increment the progress bar
-                                task.Increment(1);
-
-                                // 3.3. Slow down the process so we can see the progress...
-                                Thread.Sleep(1500);
-                            }
-                        }
-                    }
-                });
-
-            AnsiConsole.MarkupLine("[green]All bookmarks have been successfully exported.[/]");
-        }
-
-        void ViewBookmarks()
-        {
-            // Create the tree
-            var root = new Tree("Bookmarks");
-
-            // Add some nodes
-            var techBooksCategory = root.AddNode("[yellow]Tech Books[/]");
-            var carsCategory = root.AddNode("[yellow]Cars[/]");
-            var socialMediaCategory = root.AddNode("[yellow]Social Media[/]");
-            var cookingCategory = root.AddNode("[yellow]Cooking[/]");
-
-            // Add bookmarks for the Tech Books category
-            var techBooks = _bookmarkService.GetBookmarksByCategory("Tech Books");
-            foreach (var techBook in techBooks)
-            {
-                techBooksCategory.AddNode($"{techBook.Name} | {techBook.Url}");
-            }
-
-            var carsBooks = _bookmarkService.GetBookmarksByCategory("Cars");
-            foreach (var carsBook in carsBooks)
-            {
-                carsCategory.AddNode($"{carsBook.Name} | {carsBook.Url}");
-            }
-
-            var socialMediaBooks = _bookmarkService.GetBookmarksByCategory("Social Media");
-            foreach (var socialMediaBook in socialMediaBooks)
-            {
-                socialMediaCategory.AddNode($"{socialMediaBook.Name} | {socialMediaBook.Url}");
-            }
-
-            var cookingBooks = _bookmarkService.GetBookmarksByCategory("Cooking");
-            foreach (var cookingBook in cookingBooks)
-            {
-                cookingCategory.AddNode($"{cookingBook.Name} | {cookingBook.Url}");
-            }
-
-            // Render the tree
-            AnsiConsole.Write(root);
         }
     }
 
     private static void FreeSerilogLoggerOnShutdown()
     {
+        // This event is raised when the process is about to exit,
+        // allowing you to perform cleanup tasks or save data
         AppDomain.CurrentDomain.ProcessExit += (sender, eventArgs) => ExecuteShutdownTasks();
+        // This event is triggered when the user presses Ctrl+C or Ctrl+Break.
+        // While it doesn't cover all shutdown scenarios,
+        // it's useful for handling user-initiated terminations.
         Console.CancelKeyPress += (sender, eventArgs) => ExecuteShutdownTasks();
     }
 
+    // Code to execute before shutdown
     private static void ExecuteShutdownTasks()
     {
         Console.WriteLine("Performing shutdown tasks...");
