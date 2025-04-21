7ff6f8a4ddd611981d2998e2a09d5a8a3e8b5ca6
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-04-21 12:50:59 -0400

    Add unit test for LinkCommand

--- Bookmarkr.UnitTests/Commands/Link/LinkCommandTests.cs
+++ Bookmarkr.UnitTests/Commands/Link/LinkCommandTests.cs
@@ -1,6 +1,27 @@
+using BookmarkrV9.Commands.Link;
+using BookmarkrV9.Commands.Link.Add;
+using BookmarkrV9.Services.BookmarkService;
+
 namespace Bookmarkr.UnitTests.Commands.Link;
 
+[TestClass]
 public class LinkCommandTests
 {
-    
+    [TestMethod]
+    public void LinkCommand_CallingClassConstructor_EnsuresThatLinkAddCommandIsTheOnlySubCommandOfLinkCommand()
+    {
+        // Arrange
+        IBookmarkService service = null;
+        var expectedSubCommand = new LinkAddCommand(service, "add", "Add a new bookmark link");
+
+        // Act
+        var actualCommand = new LinkCommand(service, "link", "Mange bookmark links");
+        var actualSubCommand = actualCommand.Subcommands[0];
+
+        // Assert
+        Assert.AreEqual(1, actualCommand.Subcommands.Count);
+        CollectionAssert.AreEqual(actualSubCommand.Aliases.ToList(), expectedSubCommand.Aliases.ToList());
+        Assert.AreEqual(actualSubCommand.Description, expectedSubCommand.Description);
+    }
+
 }
