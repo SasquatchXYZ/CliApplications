1c1c33c2129a711a9b1506a6b886a6840d2c9d22
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-04-21 15:28:32 -0400

    Add NSubstitute and use mocking

--- Bookmarkr.UnitTests/Commands/Link/LinkCommandTests.cs
+++ Bookmarkr.UnitTests/Commands/Link/LinkCommandTests.cs
@@ -1,6 +1,7 @@
 using BookmarkrV9.Commands.Link;
 using BookmarkrV9.Commands.Link.Add;
 using BookmarkrV9.Services.BookmarkService;
+using NSubstitute;
 
 namespace Bookmarkr.UnitTests.Commands.Link;
 
@@ -11,11 +12,11 @@ public class LinkCommandTests
     public void LinkCommand_CallingClassConstructor_EnsuresThatLinkAddCommandIsTheOnlySubCommandOfLinkCommand()
     {
         // Arrange
-        IBookmarkService service = null;
-        var expectedSubCommand = new LinkAddCommand(service, "add", "Add a new bookmark link");
+        var bookmarkService = Substitute.For<IBookmarkService>();
+        var expectedSubCommand = new LinkAddCommand(bookmarkService, "add", "Add a new bookmark link");
 
         // Act
-        var actualCommand = new LinkCommand(service, "link", "Mange bookmark links");
+        var actualCommand = new LinkCommand(bookmarkService, "link", "Manage bookmark links");
         var actualSubCommand = actualCommand.Subcommands[0];
 
         // Assert
